94c46a7860a34118132720676d5c5ce8b351bd89
Manoele Ribeiro <137442585+ManoeleRibeiro@users.noreply.github.com>
2024-10-17 10:48:38 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url =  'https://github.com/ManoeleRibeiro/REDES-SOCIAIS'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
